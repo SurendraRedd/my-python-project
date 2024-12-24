cc83eb4ba79ceb49e365bf908f6907792c364879
Surendra Reddy <37290660+SurendraRedd@users.noreply.github.com>
2024-12-24 16:03:17 +0530

    Update example.py

--- src/example.py
+++ src/example.py
@@ -1,6 +1,6 @@
 # src/example.py
 
 def example_function():
-  print( "Hello, world!" )  # Improper formatting and spacing
+    print( "Hello, world from ci pipeline!" )  # Improper formatting and spacing
 
 example_function()
